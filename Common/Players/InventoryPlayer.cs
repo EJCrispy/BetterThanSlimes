c21fb431579c46898b2fee70b0d1c30446efbf0a
minitoast99 <jakesterner1@gmail.com>
2024-09-16 18:11:35 -0400

    Add files via upload

---
+++ Common/Players/InventoryPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+//This section of code removes all copper tools from the starting inventory of the player, and (IN THE FUTURE, NOT IMPLEMENTED YET) grants the player the Guidebook item. 
+namespace TutorialMod.Common.Players
+{
+    public class InventoryPlayer : ModPlayer
+    {
+        public override void ModifyStartingInventory(IReadOnlyDictionary<string, List<Item>> itemsByMod, bool mediumCoreDeath)
+        {
+            itemsByMod["Terraria"].RemoveAll(item => item.type == ItemID.CopperShortsword);
+            itemsByMod["Terraria"].RemoveAll(item => item.type == ItemID.CopperPickaxe);
+            itemsByMod["Terraria"].RemoveAll(item => item.type == ItemID.CopperAxe);
+        }
+    }
+}
---
+++ Common/Players/PlayerTraits.cs
@@ -0,0 +1,30 @@
+﻿using rail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+//This section of code reduces base player movement speed, acceleration speed, max jump height, and jump speed.
+namespace TutorialMod.Common.Players
+{
+    public class PlayerTraits : ModPlayer
+    {
+        public override void PostUpdateMiscEffects()
+        {
+            base.PostUpdateMiscEffects();
+            Player.maxRunSpeed-= 1.325f;
+            Player.accRunSpeed-= 1.325f;
+            Player.jumpHeight -= -2;
+            Player.maxFallSpeed -= -10;
+            Player.jumpSpeed -= -0.0075f;
+        }
+
+
+
+    }
+}
