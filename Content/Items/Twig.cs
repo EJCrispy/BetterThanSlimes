76b9d2bef232266ca5522e860d57336fe77aaa0a
Garmon <Ljsm0007@gmail.com>
2024-09-18 13:08:29 -0500

    Added twig

---
+++ Content/Items/Twig.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes.Content.Items
+{
+    public class Twig : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 18;
+
+            Item.rare = ItemRarityID.White;
+            Item.maxStack = 9999;
+            Item.value = 0;
+        }
+    }
+}
