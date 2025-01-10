f10452a13f38b28ac1b1074f49bb6341cbf932f0
Jake Sterner <jakesterner1@gmail.com>
2025-01-09 23:54:25 -0500

    Black Steel base weapon finalized (no spirit added yet)

--- Content/Items/Weapons/BlackSteel.cs
+++ Content/Items/Weapons/BlackSteel.cs
@@ -18,9 +18,11 @@ namespace BetterThanSlimes.Content.Items.Weapons
             Item.value = 40462;
             Item.maxStack = 1;
 
+            Item.width = 40;
+            Item.height = 40;
             Item.useStyle = ItemUseStyleID.Swing;
-            Item.useAnimation = 16;
-            Item.useTime = 16;
+            Item.useAnimation = 26;
+            Item.useTime = 26;
             Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
             Item.consumable = false;
