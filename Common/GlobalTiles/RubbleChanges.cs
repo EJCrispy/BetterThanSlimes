02413b302b2a54fdbde0729de13550f7c06b0293
ejcrispy <ej@soundstasty.com>
2025-01-09 23:35:17 -0800

    Trying to do rubble shit, it wont work

--- Common/GlobalTiles/RubbleChanges.cs
+++ Common/GlobalTiles/RubbleChanges.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria;
+﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace BetterThanSlimes.Common.GlobalTiles
+public class MyModPlayer : ModPlayer
 {
-    public class RubbleChanges : GlobalTile;
-        }
-    
+    public override void ProcessTriggers(TriggersSet triggersSet)
+    {
+        if (Main.mouseRight)
+        {
+            int i = Player.tileTargetX;
+            int j = Player.tileTargetY;
+            Tile tile = Main.tile[i, j];
 
+            // Check for a specific tile frame that corresponds to rubble tiles
+            if (tile != null && tile.TileFrameX == 144 && tile.TileFrameY == 108)
+            {
+                WorldGen.KillTile(i, j, noItem: true);
+                for (int k = 0; k < 10; k++)
+                {
+                    Item.NewItem(new EntitySource_TileInteraction(Player, i, j), i * 16, j * 16, 16, 16, ItemID.StoneBlock);
+                }
+            }
+        }
+    }
+}
