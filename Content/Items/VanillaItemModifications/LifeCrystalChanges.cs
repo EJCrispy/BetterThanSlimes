e9737fb4de843efc076b5fb2522465a00c0a0b42
ejcrispy <ej@soundstasty.com>
2025-01-09 19:51:35 -0800

    Trying something new

--- Content/Items/VanillaItemModifications/LifeCrystalChanges.cs
+++ Content/Items/VanillaItemModifications/LifeCrystalChanges.cs
@@ -13,17 +13,24 @@ namespace BetterThanSlimes.Content.Items.VanillaItemModifications
             return item.type == ItemID.LifeCrystal;
         }
 
-        public override void SetDefaults(Item item)
+        public override bool CanUseItem(Item item, Player player)
         {
-            item.StatsModifiedBy.Add(Mod); // Notify the game that we've made a functional change to this item.
+            if (player.statLifeMax < 500)
+            {
+                player.statLifeMax2 += LifePerFruit;
+                player.statLife += LifePerFruit;
+                return true;
+            }
+            return false;
         }
 
         public override void OnConsumeItem(Item item, Player player)
         {
-            if (player.statLifeMax < 500)
+            if (item.type == ItemID.LifeCrystal && player.statLifeMax < 500)
             {
                 player.statLifeMax += LifePerFruit;
                 player.statLife += LifePerFruit;
+                Main.NewText("Increased max life by 10!", 255, 240, 20);
             }
         }
     }
