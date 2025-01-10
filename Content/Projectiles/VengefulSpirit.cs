d87305612fb377d064f690221f9e7dea34b0b26d
ejcrispy <ej@soundstasty.com>
2025-01-10 11:25:53 -0800

    Fix

--- Content/Projectiles/VengefulSpirit.cs
+++ Content/Projectiles/VengefulSpirit.cs
@@ -19,7 +19,7 @@ namespace BetterThanSlimes.Content.Projectiles
             Projectile.friendly = true;
             Projectile.hostile = false;
             Projectile.DamageType = DamageClass.Melee;
-            Projectile.penetrate = 300;
+            Projectile.penetrate = 4;
             Projectile.timeLeft = 1600;
             Projectile.light = 0.5f;
             Projectile.extraUpdates = 1;
