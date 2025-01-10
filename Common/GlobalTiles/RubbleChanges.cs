4d652bb1897a4d19bf817fe437e707bdd33770b8
Jake Sterner <jakesterner1@gmail.com>
2025-01-10 13:15:30 -0500

    Adds RubbleChanges stuff NOT IMPORTANT

--- Common/GlobalTiles/RubbleChanges.cs
+++ Common/GlobalTiles/RubbleChanges.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BetterThanSlimes.Content.Items.Weapons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,15 @@ using Terraria.ModLoader;
 
 namespace BetterThanSlimes.Common.GlobalTiles
 {
-    public class RubbleChanges : GlobalTile;
-        }
-    
+    public class RubbleChanges : ModTile
+    {
+        public override void MouseOver(int i, int j)
+        {
 
+            // Show a little loose stone icon on the mouse indicating you're hovering over it.
+
+            Main.LocalPlayer.cursorItemIconEnabled = true;
+            Main.LocalPlayer.cursorItemIconID = ModContent.ItemType<LooseStone>();
+        }
+    }
+}
