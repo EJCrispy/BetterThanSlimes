77bd214f94b9a59881affcdcf10c29211deb3f8c
Jake Sterner <jakesterner1@gmail.com>
2025-01-09 22:44:18 -0500

    Adds: Black Steel BlackSteel.png GlobalWeaponModifications (i removedd this i think kk>?)

--- Common/Players/PlayerTraits.cs
+++ Common/Players/PlayerTraits.cs
@@ -9,19 +9,20 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 
-//This section of code reduces base player movement speed, acceleration speed, max jump height, and jump speed.
 namespace TutorialMod.Common.Players
 {
     public class PlayerTraits : ModPlayer
     {
+        //This section of code reduces base player movement speed, acceleration speed, max jump height, and jump speed.
         public override void PostUpdateMiscEffects()
         {
             base.PostUpdateMiscEffects();
-            Player.maxRunSpeed-= 1.325f;
-            Player.accRunSpeed-= 1.325f;
-            Player.jumpHeight += 2;
+            Player.maxRunSpeed -= 1.325f;
+            Player.accRunSpeed -= 1.325f;
+            Player.jumpHeight -= 2;
             Player.maxFallSpeed -= -10;
             Player.jumpSpeed -= -0.0075f;
+
         }
     }
 }
