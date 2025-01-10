6cf53a7df22039a5ae323f7e3087f76c3ae6c254
ejcrispy <ej@soundstasty.com>
2025-01-09 22:48:05 -0800

    ERB

--- Content/Items/VanillaItemModifications/Consumables/Lifecrystalamounts.cs
+++ Content/Items/VanillaItemModifications/Consumables/Lifecrystalamounts.cs
@@ -1,39 +1,34 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
-using MonoMod.Cil;
-using MonoMod.RuntimeDetour;
-using Mono.Cecil;
+using Terraria.ID;
 
 namespace BetterThanSlimes.Content.Items.VanillaItemModifications.Consumables
 {
     public class LifeCrystalDetour : GlobalItem
     {
-        private ILHook ilHook;
-
         public override bool InstancePerEntity => true;
 
-        public override void Load()
+        public override void SetDefaults(Item item)
         {
-            ilHook = new ILHook(
-                typeof(Player).GetMethod("ConsumeItem", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public),
-                new ILContext.Manipulator(ConsumeItemDetour)
-            );
-        }
-
-        public override void Unload()
-        {
-            ilHook?.Dispose();
-        }
-
-        private void ConsumeItemDetour(ILContext il)
-        {
-            var c = new ILCursor(il);
-
-            // Find the spot where the game sets the player's max life
-            if (c.TryGotoNext(i => i.MatchLdcI4(20)))
+            if (item.type == ItemID.LifeCrystal)
             {
-                c.Next.Operand = 10; // Change 20 to 10
+                item.healLife = 10; // Change the heal amount to 10
             }
+        }
+
+        public override bool? UseItem(Item item, Player player)
+        {
+            if (item.type == ItemID.LifeCrystal && player.statLifeMax2 < 500)
+            {
+                player.statLifeMax2 += 10; // Increase max life by 10
+                player.statLife += 10; // Also increase current life by 10
+                if (Main.myPlayer == player.whoAmI)
+                {
+                    player.HealEffect(10, true); // Show the healing effect
+                }
+                return true; // Prevent default behavior
+            }
+            return base.UseItem(item, player);
         }
     }
 }
