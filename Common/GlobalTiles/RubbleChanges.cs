2821667ef989533d7205a514fd3cd7202af38920
Jake Sterner <jakesterner1@gmail.com>
2025-01-10 00:00:00 -0500

    Black steel s hite

--- Common/GlobalTiles/RubbleChanges.cs
+++ Common/GlobalTiles/RubbleChanges.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace BetterThanSlimes.Common.GlobalTiles
 {
-    internal class Class1
+    public class RubbleChanges : GlobalTile
     {
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<>();
+        }
     }
 }
