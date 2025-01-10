46fa791f032df318633c284ce0861ce90d14daca
ejcrispy <ej@soundstasty.com>
2025-01-10 00:01:42 -0800

    Added shitty axe, prolly doesnt work.

--- Common/GlobalTiles/RubbleChanges.cs
+++ Common/GlobalTiles/RubbleChanges.cs
@@ -5,30 +5,4 @@ using Terraria.GameInput;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-public class DebugRubbleTiles : ModPlayer
-{
-    public override void ProcessTriggers(TriggersSet triggersSet)
-    {
-        if (Main.mouseRight)
-        {
-            int i = Player.tileTargetX;
-            int j = Player.tileTargetY;
-            Tile tile = Main.tile[i, j];
-
-            if (tile != null && IsBoulderTile(tile))
-            {
-                Console.WriteLine($"Rubble Tile Frame: X = {tile.TileFrameX}, Y = {tile.TileFrameY}");
-                WorldGen.KillTile(i, j, noItem: true);
-                for (int k = 0; k < 10; k++)
-                {
-                    Item.NewItem(new EntitySource_TileInteraction(Player, i, j), i * 16, j * 16, 16, 16, ItemID.StoneBlock);
-                }
-            }
-        }
-    }
-
-    private bool IsBoulderTile(Tile tile)
-    {
-        return tile.TileFrameX == 144 && tile.TileFrameY == 108; // 
-    }
-}
+// giving up on ts (this shit)
---
+++ Content/Items/Tools/Stone Hatchet.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes.Content.Items.Tools
+{
+    public class StoneHatchet : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 5;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 11;
+            Item.height = 11;
+            Item.useTime = 23;
+            Item.useAnimation = 23;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 3;
+            Item.value = 10;
+            Item.rare = ItemRarityID.White;
+            Item.UseSound = SoundID.Item3;
+            Item.autoReuse = true; // Automatically re-swing/re-use this item after its swinging animation is over.
+
+            Item.axe = 3; // How much axe power the weapon has, note that the axe power displayed in-game is this value multiplied by 5
+            Item.attackSpeedOnlyAffectsWeaponAnimation = true; // Melee speed affects how fast the tool swings for damage purposes, but not how fast it can dig
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+   
+            }
+        
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+           
+        }
+    }
+}
