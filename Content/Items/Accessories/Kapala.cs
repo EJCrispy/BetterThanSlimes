94079f92ad7cb9fc926d7283d5b36e2e25726f4d
ejcrispy <ej@soundstasty.com>
2025-01-10 18:47:40 -0800

    quick commit

---
+++ Content/Items/Accessories/Kapala.cs
@@ -0,0 +1,57 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Accessories
+{
+    public class Kapala : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            // Localization handled through localization file
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 14;
+            Item.accessory = true;
+            Item.value = Item.sellPrice(0, 1);
+            Item.rare = ItemRarityID.Green; // You can change the rarity if desired
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.maxMinions += 1; // Grants an additional summon slot
+            player.statLifeMax2 += 30; // Increases maximum life by 30
+            player.GetModPlayer<KapalaPlayer>().kapalaEquipped = true;
+        }
+    }
+
+    public class KapalaPlayer : ModPlayer
+    {
+        public bool kapalaEquipped = false;
+
+        public override void ResetEffects()
+        {
+            kapalaEquipped = false;
+        }
+
+        public void HealOnKill(int healAmount)
+        {
+            Player.statLife += healAmount;
+            Player.HealEffect(healAmount);
+        }
+    }
+
+    public class ExampleGlobalNPC : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (Main.LocalPlayer.GetModPlayer<KapalaPlayer>().kapalaEquipped)
+            {
+                Main.LocalPlayer.GetModPlayer<KapalaPlayer>().HealOnKill(10);
+            }
+        }
+    }
+}
