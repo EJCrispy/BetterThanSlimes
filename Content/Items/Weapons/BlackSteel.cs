1284260d8c7de0b7acd7e094bfc89bd66ac28260
ejcrispy <ej@soundstasty.com>
2025-01-13 01:15:58 -0800

    please work

--- Content/Items/Weapons/BlackSteel.cs
+++ Content/Items/Weapons/BlackSteel.cs
@@ -10,7 +10,7 @@ namespace BetterThanSlimes.Content.Items.Weapons
     {
         public override void SetDefaults()
         {
-            //Common Properties
+            // Common Properties
             Item.rare = ItemRarityID.Blue;
             Item.value = 40464;
             Item.maxStack = 1;
@@ -32,35 +32,24 @@ namespace BetterThanSlimes.Content.Items.Weapons
             Item.noMelee = false;
             Item.DamageType = DamageClass.Melee;
         }
-    }
-}
 
-namespace BetterThanSlimes.Content
-{
-    public class GlobalNPCMod : GlobalNPC
-    {
-        public override void OnKill(NPC npc)
+        public override bool? UseItem(Player player)
         {
-            Player player = Main.player[npc.lastInteraction];
-            // Check if the NPC was killed by your custom weapon and is not a critter
-            if (npc.catchItem <= 0 && player.HeldItem.type == ModContent.ItemType<Items.Weapons.BlackSteel>())
+            // Get the direction vector from the player to the mouse position
+            Vector2 direction = Main.MouseWorld - player.Center;
+            direction.Normalize();
+
+            // Update the player's direction based on the mouse position
+            if (direction.X > 0)
             {
-                int projectileID = ModContent.ProjectileType<Projectiles.VengefulSpirit>();
-                Vector2 spawnPosition = npc.Center;
-
-                // Determine the number of projectiles to spawn (between 1 and 3)
-                int numberOfProjectiles = Main.rand.Next(1, 4);
-
-                // Spawn the projectiles in random directions
-                for (int i = 0; i < numberOfProjectiles; i++)
-                {
-                    // Generate random direction for the projectile
-                    Vector2 randomDirection = new Vector2(Main.rand.NextFloat(-1, 1), Main.rand.NextFloat(-1, 1)).SafeNormalize(Vector2.UnitY);
-
-                    // Spawn the projectile with the random direction and a speed of 5
-                    Projectile.NewProjectile(npc.GetSource_Death(), spawnPosition, randomDirection * 5, projectileID, 15, 2, Main.myPlayer);
-                }
+                player.direction = 1;
             }
+            else
+            {
+                player.direction = -1;
+            }
+
+            return base.UseItem(player);
         }
     }
 }
