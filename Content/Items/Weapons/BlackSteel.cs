81171ed9e7a9609243e961e8500b717ead386b67
ejcrispy <ej@soundstasty.com>
2025-01-13 01:19:11 -0800

    an american pickle

--- Content/Items/Weapons/BlackSteel.cs
+++ Content/Items/Weapons/BlackSteel.cs
@@ -53,3 +53,33 @@ namespace BetterThanSlimes.Content.Items.Weapons
         }
     }
 }
+
+namespace BetterThanSlimes.Content
+{
+    public class GlobalNPCMod : GlobalNPC
+    {
+        public override void OnKill(NPC npc)
+        {
+            Player player = Main.player[npc.lastInteraction];
+            // Check if the NPC was killed by your custom weapon and is not a critter
+            if (npc.catchItem <= 0 && player.HeldItem.type == ModContent.ItemType<Items.Weapons.BlackSteel>())
+            {
+                int projectileID = ModContent.ProjectileType<Projectiles.VengefulSpirit>();
+                Vector2 spawnPosition = npc.Center;
+
+                // Determine the number of projectiles to spawn (between 1 and 3)
+                int numberOfProjectiles = Main.rand.Next(1, 4);
+
+                // Spawn the projectiles in random directions
+                for (int i = 0; i < numberOfProjectiles; i++)
+                {
+                    // Generate random direction for the projectile
+                    Vector2 randomDirection = new Vector2(Main.rand.NextFloat(-1, 1), Main.rand.NextFloat(-1, 1)).SafeNormalize(Vector2.UnitY);
+
+                    // Spawn the projectile with the random direction and a speed of 5
+                    Projectile.NewProjectile(npc.GetSource_Death(), spawnPosition, randomDirection * 5, projectileID, 15, 2, Main.myPlayer);
+                }
+            }
+        }
+    }
+}
