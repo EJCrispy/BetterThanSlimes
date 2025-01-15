baa6026de94d5017d9e6f29089577fc7834e1c75
ejcrispy <ej@soundstasty.com>
2025-01-15 15:41:15 -0800

    Loose Dirt?

---
+++ Content/Items/Placeable/LooseDirtItem.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+public class LooseDirtItem : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        
+    }
+
+    public override void SetDefaults()
+    {
+        Item.width = 12;
+        Item.height = 12;
+        Item.maxStack = 9999;
+        Item.value = 0;
+        Item.useTurn = true;
+        Item.autoReuse = true;
+        Item.useAnimation = 15;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.consumable = true;
+        Item.createTile = ModContent.TileType<LooseDirtTile>();
+    }
+}
---
+++ Content/ModdedTiles/LooseDirt.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Terraria.Localization;
+
+public class LooseDirtTile : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        Main.tileSolid[Type] = true;
+        Main.tileMergeDirt[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        AddMapEntry(new Color(151, 107, 75), Language.GetText("Loose Dirt"));
+
+        DustType = DustID.Dirt; // Determines the type of dust the tile generates when broken
+    }
+
+    public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+    {
+        if (!noItem && !effectOnly)
+        {
+            // Prevent the default item drop
+            noItem = true;
+            // Drop the custom item (Loose Dirt)
+            Item.NewItem(new Terraria.DataStructures.EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 16, ModContent.ItemType<LooseDirtItem>());
+        }
+    }
+}
