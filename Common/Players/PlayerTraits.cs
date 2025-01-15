f8aef05d9a10691fd6b6f7e47685c0eaf6b01404
ejcrispy <ej@soundstasty.com>
2025-01-14 19:30:04 -0800

    reppe

--- Common/Players/PlayerTraits.cs
+++ Common/Players/PlayerTraits.cs
@@ -19,7 +19,7 @@ namespace TutorialMod.Common.Players
             base.PostUpdateMiscEffects();
             Player.maxRunSpeed -= 1.125f;
             Player.accRunSpeed -= 1.125f;
-            Player.jumpHeight -= 0;
+            Player.jumpHeight -= 3;
             Player.maxFallSpeed -= -10;
             Player.jumpSpeed -= -0.0195f;
 
