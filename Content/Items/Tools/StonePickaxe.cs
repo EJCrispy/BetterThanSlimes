8c81ff19bd22edd063ee22a4dd8359625db26224
ejcrispy <ej@soundstasty.com>
2025-01-14 18:53:59 -0800

    LETS FUCKING GOOO ITS PLAYABLE

---
+++ Content/Items/Tools/StonePickaxe.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes.Content.Items.Tools
+{
+    public class StonePickaxe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 5;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 16;
+            Item.height = 16;
+            Item.useTime = 52;
+            Item.useAnimation = 52;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 2;
+            Item.value = 10;
+            Item.rare = ItemRarityID.White;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = false;
+
+            Item.pick = 1; 
+            Item.attackSpeedOnlyAffectsWeaponAnimation = true; // Melee speed affects how fast the tool swings for damage purposes, but not how fast it can dig
+            Item.tileBoost = -3; // Set the range to -3
+        }
+
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            // Add any melee effects you want to apply here
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Gel, 2);
+            recipe.AddIngredient(ItemID.Wood, 3);
+            recipe.AddIngredient(5552, 2); // 5552 is loose stone
+            recipe.Register();
+        }
+    }
+}
