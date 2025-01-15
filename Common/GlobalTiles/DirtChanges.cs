eb96b80bb6461ca56754d02922b73f189df065f6
ejcrispy <ej@soundstasty.com>
2025-01-15 15:05:48 -0800

    testing tile replacement

---
+++ Common/GlobalTiles/DirtChanges.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+public class GlobalDirtTile : GlobalTile
+{
+    public override void KillTile(int i, int j, int type, ref bool fail, ref bool effectOnly, ref bool noItem)
+    {
+        if (type == TileID.Dirt)
+        {
+            // Cancel the default item drop
+            noItem = true;
+            // Drop wood instead
+            Item.NewItem(new Terraria.DataStructures.EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 16, ItemID.Wood);
+        }
+    }
+}
