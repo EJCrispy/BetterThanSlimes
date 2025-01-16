56726f04b15883310dead40d16b56be74f71cbdf
ejcrispy <ej@soundstasty.com>
2025-01-15 16:33:30 -0800

    LFGGGG

--- Content/ModdedTiles/LooseDirtTile.cs
+++ Content/ModdedTiles/LooseDirtTile.cs
@@ -14,9 +14,12 @@ namespace BetterThanSlimes.Content.ModdedTiles
             Main.tileSolid[Type] = true;
             Main.tileMergeDirt[Type] = true;
             Main.tileBlockLight[Type] = true;
+            Main.tileSand[Type] = true; // Enables gravity for the tile
+
             AddMapEntry(new Color(151, 107, 75), Language.GetText("Loose Dirt"));
 
             DustType = DustID.Dirt; // Determines the type of dust the tile generates when broken
+            MineResist = 0.5f; // Reduces the number of hits needed to break the tile
         }
 
         public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
@@ -26,8 +29,9 @@ namespace BetterThanSlimes.Content.ModdedTiles
                 // Prevent the default item drop
                 noItem = true;
                 // Drop not
- 
             }
         }
+
+        
     }
 }
