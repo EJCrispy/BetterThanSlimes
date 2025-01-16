8b048dcbf8b7ae6e6b96ce9d13f33a6aa4978ae7
ejcrispy <ej@soundstasty.com>
2025-01-16 14:31:21 -0800

    rabblerou

--- Content/Items/Tools/Stone Hatchet.cs
+++ Content/Items/Tools/Stone Hatchet.cs
@@ -36,8 +36,8 @@ namespace BetterThanSlimes.Content.Items.Tools
         {
             Recipe recipe = CreateRecipe();
             recipe.AddIngredient(ItemID.Gel, 1);
-            recipe.AddIngredient(5551, 2); // 5551 is twig
-            recipe.AddIngredient(5553, 1); // 5553 is loose stone
+            recipe.AddIngredient(5551, 2); // 5553 is twig
+            recipe.AddIngredient(5553, 1); // 5554 is loose stone
             recipe.Register();
         }
     }
