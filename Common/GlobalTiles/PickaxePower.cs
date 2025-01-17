1726942c3c6748b229eda4f785465e94ee1d8d88
ejcrispy <ej@soundstasty.com>
2025-01-16 17:24:49 -0800

    Hell yeah?

---
+++ Common/GlobalTiles/PickaxePower.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace YourModNamespace
+{
+    public class YourGlobalTile : GlobalTile
+    {
+        public override bool CanExplode(int i, int j, int type)
+        {
+            if (type == TileID.WoodBlock)
+            {
+                Player player = Main.LocalPlayer;
+                if (player.HeldItem.pick <= 100)
+                {
+                    // Prevents breaking the wood block
+                    return false;
+                }
+            }
+
+            // Allow other tiles to break normally
+            return base.CanExplode(i, j, type);
+        }
+    }
+}
