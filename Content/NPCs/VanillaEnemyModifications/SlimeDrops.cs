0da073074294111f083c5b10d751c64766bfebd9
ejcrispy <ej@soundstasty.com>
2025-01-19 21:27:06 -0800

    bro

--- Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
+++ Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
@@ -1,16 +1,28 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.GameContent.ItemDropRules;
 using BetterThanSlimes.Content.Items.Materials;
+using Terraria.GameContent.ItemDropRules;
 
 namespace BetterThanSlimes.Content.NPCs.VanillaEnemyModifications
 {
     public class SlimeDrops : GlobalNPC
     {
+        public bool isRedSlime = false;
+
+        public override bool InstancePerEntity => true;
+
+        public override void SetDefaults(NPC npc)
+        {
+            if (npc.type == NPCID.RedSlime)
+            {
+                isRedSlime = true;
+            }
+        }
+
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
         {
-            if (npc.type == NPCID.BlueSlime && npc.type == -8) // Ensuring both identifiers are met, with the order swapped
+            if (isRedSlime)
             {
                 npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<RedGel>(), 1, 1, 2)); // Drops 1-2 RedGel items
             }
