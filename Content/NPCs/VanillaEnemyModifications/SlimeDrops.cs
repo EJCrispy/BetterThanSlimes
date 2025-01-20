2c391700f12be26ff672acddacf78328fe456c32
ejcrispy <ej@soundstasty.com>
2025-01-20 14:11:38 -0800

    pinky?

--- Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
+++ Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
@@ -8,21 +8,9 @@ namespace BetterThanSlimes.Content.NPCs.VanillaEnemyModifications
 {
     public class SlimeDrops : GlobalNPC
     {
-        public bool isRedSlime = false;
-
-        public override bool InstancePerEntity => true;
-
-        public override void SetDefaults(NPC npc)
-        {
-            if (npc.type == NPCID.Pinky)
-            {
-                isRedSlime = true;
-            }
-        }
-
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
         {
-            if (isRedSlime)
+            if (npc.type == NPCID.Pinky)
             {
                 npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<RedGel>(), 1, 1, 2)); // Drops 1-2 RedGel items
             }
