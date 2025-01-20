73e175d55c9f203473bd7364925ccb511ac34f7b
ejcrispy <ej@soundstasty.com>
2025-01-19 20:47:32 -0800

    holy W

--- Content/Items/Materials/RedGel.cs
+++ Content/Items/Materials/RedGel.cs
@@ -15,5 +15,10 @@ namespace BetterThanSlimes.Content.Items.Materials
             Item.maxStack = 9999;
             Item.value = 0;
         }
+
+        public override void UpdateInventory(Player player)
+        {
+            player.AddBuff(BuffID.OnFire, 1); // This sets the player on fire for 1 second (60 ticks) while the item is in their inventory
+        }
     }
 }
--- Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
+++ Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
@@ -1,1 +1,20 @@
-﻿// do ts (this) later
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+using BetterThanSlimes.Content.Items.Materials;
+
+namespace BetterThanSlimes.Content.NPCs
+{
+    public class RedSlime : ModNPC
+    {
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            if (NPC.type == NPCID.RedSlime)
+            {
+                npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<RedGel>(), 1, 1, 2)); // Drops 1-2 RedGel items
+            }
+        }
+    }
+}
