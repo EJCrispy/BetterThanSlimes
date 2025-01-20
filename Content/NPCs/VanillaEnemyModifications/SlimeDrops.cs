6e026563dda20978bdb384e91ebb6ccfe8441cd9
ejcrispy <ej@soundstasty.com>
2025-01-19 21:03:39 -0800

    LFG?

--- Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
+++ Content/NPCs/VanillaEnemyModifications/SlimeDrops.cs
@@ -10,7 +10,7 @@ namespace BetterThanSlimes.Content.NPCs.VanillaEnemyModifications
     {
         public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
         {
-            if (npc.type == 1) // Check for Red Slime using ID -8
+            if (npc.type == -8 || npc.type == NPCID.RedSlime)
             {
                 npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<RedGel>(), 1, 1, 2)); // Drops 1-2 RedGel items
             }
