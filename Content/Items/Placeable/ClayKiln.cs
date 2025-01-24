36a8908c3e627b05dd15bcaa2bcd50d065ba12d5
ejcrispy <ej@soundstasty.com>
2025-01-24 13:10:20 -0800

    hawk tuah!

---
+++ Content/Items/Placeable/ClayKiln.cs
@@ -0,0 +1,35 @@
+﻿using BetterThanSlimes.Content.Items.Materials;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Items.Placeable.Furniture
+{
+    public class ClayKiln : ModItem
+    {
+        public override void SetDefaults()
+        {
+            // ModContent.TileType<Tiles.Furniture.ExampleWorkbench>() retrieves the id of the tile that this item should place when used.
+            // DefaultToPlaceableTile handles setting various Item values that placeable items use
+            // Hover over DefaultToPlaceableTile in Visual Studio to read the documentation!
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.ClayKiln>());
+            Item.width = 28; // The item texture's width
+            Item.height = 14; // The item texture's height
+            Item.value = 150;
+        }
+
+        public override void ModifyResearchSorting(ref ContentSamples.CreativeHelper.ItemGroup itemGroup)
+        {
+            itemGroup = ContentSamples.CreativeHelper.ItemGroup.CraftingObjects;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.WorkBench)
+                .AddIngredient<RedGel>(10)
+                .Register();
+        }
+    }
+}
