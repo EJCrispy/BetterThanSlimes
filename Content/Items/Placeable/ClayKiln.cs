d1b9572d6ba432ab959296bce97782676ff91c13
ejcrispy <ej@soundstasty.com>
2025-01-24 13:17:17 -0800

    goob

--- Content/Items/Placeable/ClayKiln.cs
+++ Content/Items/Placeable/ClayKiln.cs
@@ -3,16 +3,16 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace BetterThanSlimes.Content.Items.Placeable.Furniture
+namespace BetterThanSlimes.Content.Items.Placeable.Tiles.Furniture
 {
-    public class ClayKiln : ModItem
+    public class ExampleWorkbench : ModItem
     {
         public override void SetDefaults()
         {
             // ModContent.TileType<Tiles.Furniture.ExampleWorkbench>() retrieves the id of the tile that this item should place when used.
             // DefaultToPlaceableTile handles setting various Item values that placeable items use
             // Hover over DefaultToPlaceableTile in Visual Studio to read the documentation!
-            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.ClayKiln>());
+          //  Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.ExampleWorkbench>());
             Item.width = 28; // The item texture's width
             Item.height = 14; // The item texture's height
             Item.value = 150;
