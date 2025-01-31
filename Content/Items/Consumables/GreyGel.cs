9b0d588651ce740f64dd0ae2b861adc8618dd06b
ejcrispy <ej@soundstasty.com>
2025-01-31 12:18:25 -0800

    lol

--- Content/Items/Consumables/GreyGel.cs
+++ Content/Items/Consumables/GreyGel.cs
@@ -5,46 +5,45 @@ using Microsoft.Xna.Framework;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace BetterThanSlimes.Content.Items.Consumables;
-
-public class GreyGel : ModItem
+namespace BetterThanSlimes.Content.Items.Consumables
 {
-    public override void SetStaticDefaults()
+    public class SummonSlimeRain : ModItem
     {
-
-    }
-
-    public override void SetDefaults()
-    {
-        Item.width = 20;
-        Item.height = 20;
-        Item.maxStack = 99;
-        Item.useTime = 20;
-        Item.useAnimation = 20;
-        Item.useStyle = ItemUseStyleID.HoldUp;
-        Item.UseSound = SoundID.Item44;
-        Item.consumable = true;
-    }
-
-    public override bool? UseItem(Player player)
-    {
-        // Only useable during rain
-        if (!Main.raining) return false;
-
-        // Check if slime rain is not already happening
-        if (!Main.slimeRain)
+        public override void SetStaticDefaults()
         {
-            Main.StartSlimeRain(true); // Start slime rain
-            Main.NewText("The Slime Rain has begun!", 50, 255, 130);
         }
-        return true;
-    }
 
-    public override void AddRecipes()
-    {
-        Recipe recipe = CreateRecipe();
-        recipe.AddIngredient(ItemID.Gel, 50); // Requires 50 gel
-        recipe.AddTile(TileID.WorkBenches); // Crafted at a workbench
-        recipe.Register();
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 99;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.UseSound = SoundID.Item44;
+            Item.consumable = true;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            // Only useable during rain
+            if (!Main.raining) return false;
+
+            // Check if slime rain is not already happening
+            if (!Main.slimeRain)
+            {
+                Main.StartSlimeRain(true); // Start slime rain
+            }
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Gel, 50); // Requires 50 gel
+            recipe.AddTile(TileID.WorkBenches); // Crafted at a workbench
+            recipe.Register();
+        }
     }
 }
