3970f2823f51911de4e7631832b2a768db2ef9b8
Jake Sterner <jakesterner1@gmail.com>
2025-02-21 00:08:47 -0500

    hi there breain of confusion changes are here

--- Common/VanillaItemChanges/Accessories/BrainOfConfusionChanges.cs
+++ Common/VanillaItemChanges/Accessories/BrainOfConfusionChanges.cs
@@ -1,6 +1,8 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
+using System.Collections.Generic;
 
 namespace BetterThanSlimes.Common.VanillaItemChanges.Accessories
 {
@@ -16,6 +18,19 @@ namespace BetterThanSlimes.Common.VanillaItemChanges.Accessories
 
                 // Track whether the player is wearing the Brain of Confusion
                 player.GetModPlayer<BrainOfConfusionPlayer>().hasBrainOfConfusion = true;
+            }
+        }
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            // Check if the item is the Brain of Confusion
+            if (item.type == ItemID.BrainOfConfusion)
+            {
+                // Create a new tooltip line
+                TooltipLine newTooltip = new TooltipLine(Mod, "BrainOfConfusionDebuff", "When hit, you now become confused and distorted");
+
+                // Add the new tooltip line to the list of tooltips
+                tooltips.Add(newTooltip);
             }
         }
     }
@@ -37,8 +52,7 @@ namespace BetterThanSlimes.Common.VanillaItemChanges.Accessories
             {
                 // Apply debuffs to the player when the dodge procs
                 Player.AddBuff(BuffID.Confused, 25); // Confused debuff for 1 second (60 frames)
-                Player.AddBuff(BuffID.VortexDebuff, 18);
-
+                Player.AddBuff(BuffID.VortexDebuff, 18); // Vortex Debuff for 18 frames
             }
             else
             {
@@ -53,7 +67,7 @@ namespace BetterThanSlimes.Common.VanillaItemChanges.Accessories
             {
                 // Apply debuffs to the player when the dodge procs
                 Player.AddBuff(BuffID.Confused, 25); // Confused debuff for 1 second (60 frames)
-                Player.AddBuff(BuffID.VortexDebuff, 18);
+                Player.AddBuff(BuffID.VortexDebuff, 18); // Vortex Debuff for 18 frames
             }
             else
             {
