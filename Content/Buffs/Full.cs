161f1799753d789c007e67f246329c5978e30aaa
ejcrispy <ej@soundstasty.com>
2025-02-20 18:14:04 -0800

    hungruzz

---
+++ Content/Buffs/Full.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes.Content.Buffs
+{
+    public class Full : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = false; // This is not a debuff
+            Main.buffNoSave[Type] = true; // This buff won't save when exiting and rejoining the world
+            Main.buffNoTimeDisplay[Type] = false; // Time will be displayed for this buff
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            // When the buff is about to run out, apply the Starvation debuff
+            if (player.buffTime[buffIndex] == 1) // 1 frame before the buff expires
+            {
+                player.AddBuff(ModContent.BuffType<Starvation>(), 60 * 60); // Apply Starvation for 60 seconds
+            }
+        }
+    }
+}
---
+++ Content/Buffs/Starvation.cs
@@ -0,0 +1,32 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes.Content.Buffs
+{
+    public class Starvation : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true; // This is a debuff
+            Main.buffNoSave[Type] = true; // This buff won't save when exiting and rejoining the world
+            Main.buffNoTimeDisplay[Type] = false; // Time will be displayed for this buff
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            // Gradually reduce the player's health
+            if (player.lifeRegen > 0)
+            {
+                player.lifeRegen = 0;
+            }
+            player.lifeRegenTime = 0;
+            player.lifeRegen -= 20; // Adjust this value to control how fast the player loses health
+
+            // Optionally, kill the player if their health reaches 0
+            if (player.statLife <= 0)
+            {
+                player.KillMe(Terraria.DataStructures.PlayerDeathReason.ByCustomReason(player.name + " starved to death."), 1, 0);
+            }
+        }
+    }
+}
