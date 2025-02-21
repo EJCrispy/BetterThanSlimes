2d682708041acc67d7968c00c503aaa895e4d12f
ejcrispy <ej@soundstasty.com>
2025-02-20 19:59:37 -0800

    ribvlit

--- Content/Buffs/Full.cs
+++ Content/Buffs/Full.cs
@@ -14,12 +14,6 @@ namespace BetterThanSlimes.Content.Buffs
 
         public override void Update(Player player, ref int buffIndex)
         {
-            // When the buff is about to run out, apply the Starvation debuff permanently
-            if (player.buffTime[buffIndex] == 1) // 1 frame before the buff expires
-            {
-                player.AddBuff(ModContent.BuffType<Starvation>(), int.MaxValue); // Apply Starvation permanently
-            }
-
             // Remove the Starvation debuff if it's active
             if (player.buffTime[buffIndex] > 1) // While the Full buff is active
             {
