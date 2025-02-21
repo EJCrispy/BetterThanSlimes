9d4f15396b3e7b816741cdbcbbc98754a37989f4
ejcrispy <ej@soundstasty.com>
2025-02-20 21:34:13 -0800

    LFG!

--- Content/NPCs/VanillaEnemyModifications/CritterModifications.cs
+++ Content/NPCs/VanillaEnemyModifications/CritterModifications.cs
@@ -17,30 +17,26 @@ namespace BetterThanSlimes.Content.NPCs.VanillaEnemyModifications
         {
             public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
             {
-                if (npc.netID == NPCID.Bunny)
-                if (npc.netID == NPCID.Squirrel)
-                if (npc.netID == NPCID.SquirrelRed)
-                if (npc.netID == NPCID.PartyBunny)
-                if (npc.netID == NPCID.BunnySlimed)
-                if (npc.netID == NPCID.BunnyXmas)
-                                    if (npc.netID == NPCID.Duck)
-                                        if (npc.netID == NPCID.Duck2)
-                                            if (npc.netID == NPCID.DuckWhite)
-                                                if (npc.netID == NPCID.DuckWhite2)
-                                                    if (npc.netID == NPCID.Grebe)
-                                                        if (npc.netID == NPCID.Grebe2)
-                                                            if (npc.netID == NPCID.Owl)
-                                                                if (npc.netID == NPCID.Penguin)
-                                                                    if (npc.netID == NPCID.Seagull)
-                                                                        if (npc.netID == NPCID.Goldfish)
-                                                                            if (npc.netID == NPCID.GoldfishWalker)
-
-
-
-
-                                                                            {
-
-                    // Add the new loot rule
+               
+if (npc.netID == NPCID.Bunny)
+if (npc.netID == NPCID.Squirrel)
+if (npc.netID == NPCID.SquirrelRed)
+if (npc.netID == NPCID.PartyBunny)
+if (npc.netID == NPCID.BunnySlimed)
+if (npc.netID == NPCID.BunnyXmas)
+if (npc.netID == NPCID.Duck)
+if (npc.netID == NPCID.Duck2)
+if (npc.netID == NPCID.DuckWhite)
+if (npc.netID == NPCID.DuckWhite2)
+if (npc.netID == NPCID.Grebe)
+if (npc.netID == NPCID.Grebe2)
+if (npc.netID == NPCID.Owl)
+if (npc.netID == NPCID.Penguin)
+if (npc.netID == NPCID.Seagull)
+if (npc.netID == NPCID.Goldfish)
+if (npc.netID == NPCID.GoldfishWalker)
+{ 
+// Add the new loot rule
                     npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Morsel>(), 1, 1, 10));
 
                 }
