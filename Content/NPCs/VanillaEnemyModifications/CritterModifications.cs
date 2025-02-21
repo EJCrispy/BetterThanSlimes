f0d99cbe0dcb6269f4133e40e2d4801260abeccd
ejcrispy <ej@soundstasty.com>
2025-02-20 21:29:12 -0800

    why is my code so good?

---
+++ Content/NPCs/VanillaEnemyModifications/CritterModifications.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using BetterThanSlimes.Content.Items.Materials;
+using BetterThanSlimes.Content.Items.Consumables;
+
+namespace BetterThanSlimes.Content.NPCs.VanillaEnemyModifications
+{ 
+
+    public class CritterModificaitons : GlobalNPC
+        {
+            public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+            {
+                if (npc.netID == NPCID.Bunny)
+                if (npc.netID == NPCID.Squirrel)
+                if (npc.netID == NPCID.SquirrelRed)
+                if (npc.netID == NPCID.PartyBunny)
+                if (npc.netID == NPCID.BunnySlimed)
+                if (npc.netID == NPCID.BunnyXmas)
+                                    if (npc.netID == NPCID.Duck)
+                                        if (npc.netID == NPCID.Duck2)
+                                            if (npc.netID == NPCID.DuckWhite)
+                                                if (npc.netID == NPCID.DuckWhite2)
+                                                    if (npc.netID == NPCID.Grebe)
+                                                        if (npc.netID == NPCID.Grebe2)
+                                                            if (npc.netID == NPCID.Owl)
+                                                                if (npc.netID == NPCID.Penguin)
+                                                                    if (npc.netID == NPCID.Seagull)
+                                                                        if (npc.netID == NPCID.Goldfish)
+                                                                            if (npc.netID == NPCID.GoldfishWalker)
+
+
+
+
+                                                                            {
+                    // Clear the existing loot rules
+                    npcLoot.RemoveWhere(rule => true);
+                    // Add the new loot rule
+                    npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Morsel>(), 1, 1, 1));
+
+                }
+            }
+        }
+    }
