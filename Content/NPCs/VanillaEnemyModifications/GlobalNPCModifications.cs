f91620d39fe0c4d77627c14061453814a6ec1f1e
ejcrispy <ej@soundstasty.com>
2025-02-20 21:49:12 -0800

    biggy

--- Content/NPCs/VanillaEnemyModifications/GlobalNPCModifications.cs
+++ Content/NPCs/VanillaEnemyModifications/GlobalNPCModifications.cs
@@ -79,7 +79,27 @@ namespace BetterThanSlimes.Content.NPCs.VanillaEnemyModifications
                     npcLoot.Add(ItemDropRule.Common(itemId: 5546,
                                                     1)); //should make clowns drop red nose
                 }
-            }
+            if (npc.type == NPCID.Bunny)
+                if (npc.type == NPCID.Squirrel)
+                    if (npc.type == NPCID.SquirrelRed)
+                        if (npc.type == NPCID.PartyBunny)
+                            if (npc.type == NPCID.BunnySlimed)
+                                if (npc.type == NPCID.BunnyXmas)
+                                    if (npc.type == NPCID.Duck)
+                                        if (npc.type == NPCID.Duck2)
+                                            if (npc.type == NPCID.DuckWhite)
+                                                if (npc.type == NPCID.DuckWhite2)
+                                                    if (npc.type == NPCID.Grebe)
+                                                        if (npc.type == NPCID.Grebe2)
+                                                            if (npc.type == NPCID.Owl)
+                                                                if (npc.type == NPCID.Penguin)
+                                                                    if (npc.type == NPCID.Seagull)
+                                                                        if (npc.type == NPCID.Goldfish)
+                                                                            if (npc.type == NPCID.GoldfishWalker)
+                                                                                if (npc.type == NPCID.SnowFlinx)
+                                                                                    npcLoot.Add(ItemDropRule.Common(itemId: 5546,
+                                1)); //should make clowns drop red nose
+        }
         }
     }
 
