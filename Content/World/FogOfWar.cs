d8f051da196c9f55bd2568cf6364d683bff3bf80
ejcrispy <ej@soundstasty.com>
2025-02-22 21:46:21 -0800

    RAHG

--- Content/World/FogOfWar.cs
+++ Content/World/FogOfWar.cs
@@ -9,34 +9,34 @@ namespace BetterThanSlimes
 {
     public class FogOfWar : ModSystem
     {
-        private static ILHook _updateLightDecayHook;
+        private static ILHook _updateLightingHook;
 
         public override void Load()
         {
-            // Find the UpdateLightDecay method in the LightingEngine class
-            MethodInfo updateLightDecayMethod = typeof(LightingEngine)
-                .GetMethod("UpdateLightDecay", BindingFlags.NonPublic | BindingFlags.Instance);
+            // Find the UpdateLighting method in the LightingEngine class
+            MethodInfo updateLightingMethod = typeof(LightingEngine)
+                .GetMethod("UpdateLighting", BindingFlags.Public | BindingFlags.Instance);
 
-            if (updateLightDecayMethod != null)
+            if (updateLightingMethod != null)
             {
-                // Create an IL hook for the UpdateLightDecay method
-                _updateLightDecayHook = new ILHook(updateLightDecayMethod, ModifyLightDecayValues);
-                Mod.Logger.Info("Successfully hooked into UpdateLightDecay!");
+                // Create an IL hook for the UpdateLighting method
+                _updateLightingHook = new ILHook(updateLightingMethod, ModifyLightingValues);
+                Mod.Logger.Info("Successfully hooked into UpdateLighting!");
             }
             else
             {
-                Mod.Logger.Error("Failed to find method: UpdateLightDecay");
+                Mod.Logger.Error("Failed to find method: UpdateLighting");
             }
         }
 
         public override void Unload()
         {
             // Dispose of the hook when the mod is unloaded
-            _updateLightDecayHook?.Dispose();
-            Mod.Logger.Info("Unloaded LightingEnginePatch functionality.");
+            _updateLightingHook?.Dispose();
+            Mod.Logger.Info("Unloaded FogOfWar functionality.");
         }
 
-        private void ModifyLightDecayValues(ILContext il)
+        private void ModifyLightingValues(ILContext il)
         {
             var c = new ILCursor(il);
 
@@ -45,19 +45,22 @@ namespace BetterThanSlimes
             {
                 // Replace 0.91f with 0.60f
                 c.Prev.Operand = 0.60f;
-                Mod.Logger.Info("Modified LightDecayThroughAir to 0.88f!");
+                Mod.Logger.Info("Modified LightDecayThroughAir to 0.60f!");
             }
             else
             {
                 Mod.Logger.Error("Failed to find LightDecayThroughAir assignment!");
             }
 
+            // Reset the cursor to the start of the method
+            c.Index = 0;
+
             // Find the instruction where LightDecayThroughSolid is set to 0.56f
             if (c.TryGotoNext(MoveType.After, x => x.MatchLdcR4(0.56f)))
             {
                 // Replace 0.56f with 0.01f
                 c.Prev.Operand = 0.01f;
-                Mod.Logger.Info("Modified LightDecayThroughSolid to 0.00f!");
+                Mod.Logger.Info("Modified LightDecayThroughSolid to 0.01f!");
             }
             else
             {
