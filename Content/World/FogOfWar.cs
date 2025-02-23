3471e3971711eb6ef7d171e59e19d85301f63bcd
ejcrispy <ej@soundstasty.com>
2025-02-22 18:39:24 -0800

    make the player always have blackout

---
+++ Content/World/FogOfWar.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes
+{
+    public class BlackoutPlayer : ModPlayer
+    {
+        public override void PostUpdate()
+        {
+            // Apply the Blackout debuff (Buff ID 80) to the player constantly
+            Player.AddBuff(80, 2); // 2 frames duration ensures it's constantly reapplied
+        }
+    }
+}
