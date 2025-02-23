8aebe1b4f1a3496709088aff053eb325e842a2ec
ejcrispy <ej@soundstasty.com>
2025-02-22 17:39:00 -0800

    poleaseeeee

--- Content/NPCs/VanillaEnemyModifications/NoSlimeBonusDrops.cs
+++ Content/NPCs/VanillaEnemyModifications/NoSlimeBonusDrops.cs
@@ -5,14 +5,17 @@ using MonoMod.Cil;
 using Mono.Cecil.Cil;
 using System.Reflection;
 
-namespace NoSlimeBonusDrops
+namespace YourModNamespace
 {
-    public class NoSlimeBonusDropsMod : Mod
+    public class BetterThanSlimes : GlobalNPC
     {
         private static ILHook _slimeDropRuleHook;
 
         public override void Load()
         {
+            // Call the base Load method
+            base.Load();
+
             // Find the GetDropInfo method in SlimeBodyItemDropRule
             MethodInfo method = typeof(Terraria.GameContent.ItemDropRules.SlimeBodyItemDropRule)
                 .GetMethod("GetDropInfo", BindingFlags.Instance | BindingFlags.Public);
@@ -21,19 +24,22 @@ namespace NoSlimeBonusDrops
             {
                 // Create an IL hook for the method
                 _slimeDropRuleHook = new ILHook(method, DisableSlimeBonusDrops);
-                Logger.Info("Successfully hooked into GetDropInfo!");
+                Mod.Logger.Info("Successfully hooked into GetDropInfo!");
             }
             else
             {
-                Logger.Error("Failed to find method: GetDropInfo");
+                Mod.Logger.Error("Failed to find method: GetDropInfo");
             }
         }
 
         public override void Unload()
         {
+            // Call the base Unload method
+            base.Unload();
+
             // Dispose of the hook when the mod is unloaded
             _slimeDropRuleHook?.Dispose();
-            Logger.Info("Unloaded NoSlimeBonusDrops mod.");
+            Mod.Logger.Info("Unloaded NoSlimeBonusDrops functionality.");
         }
 
         private void DisableSlimeBonusDrops(ILContext il)
@@ -42,7 +48,7 @@ namespace NoSlimeBonusDrops
 
             // Replace the entire method body with a simple return statement
             c.Emit(OpCodes.Ret); // Immediately return without executing any logic
-            Logger.Info("Disabled slime bonus drops!");
+            Mod.Logger.Info("Disabled slime bonus drops!");
         }
     }
 }
