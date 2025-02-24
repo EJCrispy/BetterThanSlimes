1395961f5e209fad5de76639b420bd9d88b71039
ejcrispy <ej@soundstasty.com>
2025-02-24 15:49:55 -0800

    spawnpool?

---
+++ Content/NPCs/Spawnpool/RedSlimeSpawnSystem.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace BetterThanSlimes.Content.NPCs
+{
+    public class RedSlimeSpawnGlobalNPC : GlobalNPC
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            // Check if the player is in the Forest biome and it's nighttime
+            if (spawnInfo.Player.ZoneForest && !Main.dayTime)
+            {
+                // Add Red Slime to the spawn pool with a 10% chance (adjust as needed)
+                if (!pool.ContainsKey(NPCID.RedSlime))
+                {
+                    pool.Add(NPCID.RedSlime, 0.5f); // 10% chance to spawn
+                }
+            }
+        }
+    }
+}
