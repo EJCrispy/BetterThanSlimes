4983416feb27ad25cae572268d68878da3f9f57e
ejcrispy <ej@soundstasty.com>
2025-02-25 14:08:11 -0800

    newbuikd

--- Common/GlobalTiles/FrameDebuggingRightClickMethod.cs
+++ Common/GlobalTiles/FrameDebuggingRightClickMethod.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace BetterThanSlimes.Common.GlobalTiles
-{
-    public class FrameDebuggingRightClickMethod : GlobalTile
-    {
-        public override void RightClick(int i, int j, int type)
-        {
-            Tile tile = Main.tile[i, j];
+//namespace BetterThanSlimes.Common.GlobalTiles
+//{
+//    public class FrameDebuggingRightClickMethod : GlobalTile
+//    {
+//        public override void RightClick(int i, int j, int type)
+//        {
+//            Tile tile = Main.tile[i, j];
 
-            Main.NewText($"Right-clicked tile at ({i}, {j}), TileType: {tile.TileType}, FrameX: {tile.TileFrameX}, FrameY: {tile.TileFrameY}");
-        }
-    }
-}
+ //           Main.NewText($"Right-clicked tile at ({i}, {j}), TileType: {tile.TileType}, FrameX: {tile.TileFrameX}, FrameY: {tile.TileFrameY}");
+ //       }
+//    }
+//}
