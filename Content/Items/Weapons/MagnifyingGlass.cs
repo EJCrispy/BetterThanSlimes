ac3989acd2d2f176a666b8ea072a5e9d2d7b12a8
Jake Sterner <jakesterner1@gmail.com>
2025-02-24 19:58:56 -0500

    holy crap

---
+++ Content/Items/Weapons/MagnifyingGlass.cs
@@ -0,0 +1,83 @@
+﻿using BetterThanSlimes.Content.Projectiles;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterThanSlimes.Content.Items.Weapons
+{
+    public class MagnifyingGlass : ModItem
+    {
+        private float Charge = 1f;
+        private bool maxChargeSoundPlayed = false;
+
+        public override void SetStaticDefaults()
+        {
+            Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 3;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 40;
+            Item.height = 18;
+            Item.useTime = 45;
+            Item.useAnimation = 45;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 0.5f;
+            Item.value = Item.sellPrice(0, 2);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.channel = true;
+            Item.noUseGraphic = true;
+            Item.autoReuse = true;
+            Item.shoot = ProjectileID.None;
+            Item.noMelee = true;
+        }
+
+        public override void HoldItem(Player player)
+        {
+            if (player.channel)
+            {
+                if (Charge < 4f)
+                {
+                    Charge += 1f / 30f;
+                    maxChargeSoundPlayed = false;
+                }
+                else if (!maxChargeSoundPlayed)
+                {
+                    SoundEngine.PlaySound(SoundID.Item82 with { Volume = 1f, Pitch = 2f }, player.position);
+                    maxChargeSoundPlayed = true;
+                }
+
+                if (player.ownedProjectileCounts[ModContent.ProjectileType<HoldingMagnifyingGlass>()] < 1)
+                {
+                    int damage = (int)(player.GetDamage(DamageClass.Ranged).ApplyTo(Item.damage));
+                    Projectile.NewProjectile(
+                        player.GetSource_ItemUse(Item),
+                        player.Center,
+                        Vector2.Zero,
+                        ModContent.ProjectileType<HoldingMagnifyingGlass>(),
+                        damage,
+                        Item.knockBack,
+                        player.whoAmI,
+                        Charge
+                    );
+                }
+            }
+            else
+            {
+                Charge = 1f;
+                maxChargeSoundPlayed = false;
+            }
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            return false;
+        }
+    }
+}
---
+++ Content/Projectiles/MagnifyingGlassBeam.cs
@@ -0,0 +1,68 @@
+﻿// Magnifying Glass Beam - Custom projectile with Last Prism attack animation and Shadowbeam-like projectile, colored orange
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BetterThanSlimes.Content.Projectiles
+{
+    public class MagnifyingGlassBeam : ModProjectile
+    {
+        private const int HitCooldown = 60; // One hit per NPC per second (60 ticks)
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1; // Infinite penetration
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = HitCooldown;
+            Projectile.extraUpdates = 3; // Smooth beam effect like Last Prism
+            Projectile.timeLeft = 300; // Beam duration
+        }
+
+        public override void AI()
+        {
+            // Beam direction and rotation
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver4;
+
+            // Add bright orange lighting
+            Lighting.AddLight(Projectile.Center, 1.0f, 0.5f, 0.0f);
+
+            // Beam particle effect (optional for extra flair)
+            if (Main.rand.NextBool(3))
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 1.5f);
+                dust.noGravity = true;
+                dust.velocity *= 0.5f;
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>("Terraria/Images/Projectile_255").Value; // Shadowbeam texture
+            Vector2 drawOrigin = new(texture.Width / 2, texture.Height / 2);
+            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+
+            Color beamColor = new Color(255, 128, 0, 200); // Bright orange with slight transparency
+
+            Main.EntitySpriteDraw(
+                texture,
+                drawPosition,
+                null,
+                beamColor,
+                Projectile.rotation,
+                drawOrigin,
+                Projectile.scale,
+                SpriteEffects.None,
+                0f
+            );
+
+            return false; // Skip default drawing
+        }
+    }
+}
